04f0116268c70ffb30d331abba7b3df34ff33fb4
maryam.bouderham <maryam.bouderham@gmail.com>
2021-09-06 09:10:38 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App () {
+  
+    return (
+      <div className="d-flex justify-content-center">
+       <h1>Hello world 😃</h1>
+      </div>
+    );
+  
+}
+
+export default App;
